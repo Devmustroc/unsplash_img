463b0e6953d53ecefa01aee1d4c0a4a7fef79110
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-11 15:49:44 +0100

    Css Bootstrap styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+}
 
 const Header = ({ title }) => {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar style={navbarStyle} variant="light">
                 <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Navbar>
     )
---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Container, Row, Col, Form, Button} from "react-bootstrap";
+
+const Search = () => {
+    return (
+        <Container className="mt-4">
+            <Row>
+                <Col>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Control placeholder="Search for new image" />
+                            </Col>
+                            <Col>
+                                <Button variant="outline-primary" type="submit">Search</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+};
+export default Search;
